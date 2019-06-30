2c6c54e030e0e5777eab85bbff18f699f146cbc3
Jarrett Ethan Singian <jarsingian@gmail.com>
2019-06-30 23:25:09 +0800

    table has been added

--- HTML/ViewAll.js
+++ HTML/ViewAll.js
@@ -1,6 +1,9 @@
 var result;
 var mainInfo;
-
+var firstEntry;
+var lastEntry;
+var prevButton = document.getElementById("prevButton");
+var nextButton = document.getElementById("nextButton");
 $(document).ready(function(){
 
     $('input[type="file"]').change(function(){
@@ -26,16 +29,61 @@ $(document).ready(function(){
 });
 
 window.onload = function(){
+  firstEntry = 0;
+  lastEntry = 9;
   $.getJSON('MainInfo.json', function (data){
     console.log("KrustyKrab has been loaded");
     mainInfo = data; 
-    console.log (mainInfo.sales[0]) 
+    console.log (mainInfo.sales[0])
+    
+    getTableData(firstEntry, lastEntry);
   });// use data to use the JSON object
   
 };
 
-function getTableData(){
-    document.getElementById("tableView")
+prevButton.addEventListener("click", prevList);
+nextButton.addEventListener("click", nextList);
+
+function prevList (){
+  if (firstEntry!=0){
+    firstEntry-=10;
+    lastEntry-=10;
+    getTableData(firstEntry, lastEntry);
+  }
+  
+}
+
+function nextList (){
+
+  if ((mainInfo.sales.length-1)-lastEntry<10){
+    firstEntry+=10;
+    lastEntry = mainInfo.sales.length-1;
+    getTableData(firstEntry, lastEntry)
+  }
+
+  else if (lastEntry!=mainInfo.sales.length){
+    firstEntry+=10;
+    lastEntry+=10;
+    getTableData(firstEntry, lastEntry);
+  }
+  
+}
+
+
+function getTableData(first, last){
+    var table = document.getElementById("tableView");
+    var dateTime;
+    table.innerHTML = "<tr><th>Time</th><th>Species</th><th>Burger</th></tr>";
+    
+    for (i=first; i</*mainInfo.sales.length*/last; i++){
+      dateTime = new Date (mainInfo.sales[i].datetime);
+      if (dateTime.getMinutes()>=10)
+          table.innerHTML+= "<tr><th>"+dateTime.getHours()+":" + dateTime.getMinutes()+"</th> <th> "+mainInfo.sales[i].species+"</th> <th> "+mainInfo.sales[i].burger+"</th></tr>";
+      else
+      table.innerHTML+= "<tr><th>"+dateTime.getHours()+":0" + dateTime.getMinutes()+"</th> <th> "+mainInfo.sales[i].species+"</th> <th> "+mainInfo.sales[i].burger+"</th></tr>";
+
+    }
+
 }
 
 
