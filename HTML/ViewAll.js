7259b71d738e3dbabe39261d46cfec45e5c738d8
Jarrett Ethan Singian <jarsingian@gmail.com>
2019-07-01 17:32:49 +0800

    Added a filter

--- HTML/ViewAll.js
+++ HTML/ViewAll.js
@@ -2,8 +2,10 @@ var result;
 var mainInfo;
 var firstEntry;
 var lastEntry;
+var filteredData = [];
 var prevButton = document.getElementById("prevButton");
 var nextButton = document.getElementById("nextButton");
+var findDateButton = document.getElementById("findDateButton");
 $(document).ready(function(){
 
     $('input[type="file"]').change(function(){
@@ -26,6 +28,7 @@ $(document).ready(function(){
         
 
     });
+
 });
 
 window.onload = function(){
@@ -36,13 +39,15 @@ window.onload = function(){
     mainInfo = data; 
     console.log (mainInfo.sales[0])
     
-    getTableData(firstEntry, lastEntry);
+    getAllData();
   });// use data to use the JSON object
   
 };
 
 prevButton.addEventListener("click", prevList);
 nextButton.addEventListener("click", nextList);
+findDateButton.addEventListener("click", getDataByDay)
+
 
 function prevList (){
   if (firstEntry!=0){
@@ -55,9 +60,12 @@ function prevList (){
 
 function nextList (){
 
-  if ((mainInfo.sales.length-1)-lastEntry<10){
+
+  if (filteredData.length-1==lastEntry)
+    alert("End of the line");
+  else if ((filteredData.length-1)-lastEntry<10){
     firstEntry+=10;
-    lastEntry = mainInfo.sales.length-1;
+    lastEntry = filteredData.length-1;
     getTableData(firstEntry, lastEntry)
   }
 
@@ -69,21 +77,52 @@ function nextList (){
   
 }
 
-
 function getTableData(first, last){
     var table = document.getElementById("tableView");
     var dateTime;
     table.innerHTML = "<tr><th>Time</th><th>Species</th><th>Burger</th></tr>";
     
-    for (i=first; i</*mainInfo.sales.length*/last; i++){
-      dateTime = new Date (mainInfo.sales[i].datetime);
+    for (i=first; i</*mainInfo.sales.length*/last+1; i++){
+      dateTime = new Date (filteredData[i].datetime);
       if (dateTime.getMinutes()>=10)
-          table.innerHTML+= "<tr><th>"+dateTime.getHours()+":" + dateTime.getMinutes()+"</th> <th> "+mainInfo.sales[i].species+"</th> <th> "+mainInfo.sales[i].burger+"</th></tr>";
+          table.innerHTML+= "<tr><th>"+dateTime.getHours()+":" + dateTime.getMinutes()+"</th> <th> "+filteredData[i].species+"</th> <th> "+filteredData[i].burger+"</th></tr>";
       else
-      table.innerHTML+= "<tr><th>"+dateTime.getHours()+":0" + dateTime.getMinutes()+"</th> <th> "+mainInfo.sales[i].species+"</th> <th> "+mainInfo.sales[i].burger+"</th></tr>";
+      table.innerHTML+= "<tr><th>"+dateTime.getHours()+":0" + dateTime.getMinutes()+"</th> <th> "+filteredData[i].species+"</th> <th> "+filteredData[i].burger+"</th></tr>";
 
     }
 
 }
+
+function getAllData (){
+  firstEntry = 0;
+  lastEntry = 9;
+  filteredData = [];
+  for (i = 0; i<mainInfo.sales.length; i++){
+
+    filteredData.push(mainInfo.sales[i]);
+  }
+
+  getTableData(firstEntry, lastEntry);
+
+}
+
+function getDataByDay (){
+  firstEntry = 0;
+  lastEntry = 9;
+    filteredData = [];
+    var chosenDate = new Date ( document.getElementById("dateInput").value);
+    for (i = 0; i<mainInfo.sales.length; i++){
+      jsonDateTime = new Date (mainInfo.sales[i].datetime);
+      if (chosenDate.getDate()==jsonDateTime.getDate()&&chosenDate.getMonth()==jsonDateTime.getMonth()&&chosenDate.getYear()==jsonDateTime.getYear())
+      {
+        filteredData.push(mainInfo.sales[i]);
+      }
+
+    }
+    console.log(filteredData.length+ "Entries");
+    getTableData(firstEntry, lastEntry);
+}
+
+
 
 
